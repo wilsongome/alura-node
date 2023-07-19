63db5af23ff9b01f153e927ea24e0fee012516cf
Wilson <wilsongome@gmail.com>
2023-07-19 16:05:40 -0300

    Eliminando métodos desnecessários em app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/DbConnect.js";
-import livros from "./models/livro.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão!"))
@@ -13,36 +12,5 @@ const app = express();
 app.use(express.json())
 
 routes(app)
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
-
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo =  req.body.titulo;
-    res.json(livros);
-})
-
-app.patch('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo =  req.body.titulo;
-    res.json(livros);
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso!`);
-})
-
-
-function buscaLivro(id)
-{
-    return livros.findIndex(livro => livro.id == id)
-}
-
 
 export default app
